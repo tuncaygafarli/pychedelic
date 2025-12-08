0097439823a72b5019cd8b8fcef118073090b1eb
Tuncay <qtuncay40@gmail.com>
2025-12-08 11:29:15 +0400

    deleted unnecessary importings from effects folder

--- scripts/listFunctions.py
+++ scripts/listFunctions.py
@@ -6,17 +6,11 @@ import sys
 import os
 from datetime import datetime
 
-from effects.tracker import Tracker
-from effects.color_chaos_manipulator import ColorChaosManipulator
-from effects.vhs import VHS
-
 from effects.effect_manager import EffectManager
-
-from processors.render_processor import RenderProcessor
 
 def listFunctions(args):
     effectManager = EffectManager()
 
     for effect, functions in effectManager.effects_functions.items():
         print(f"{effect} - {functions}\n")
-        
+
