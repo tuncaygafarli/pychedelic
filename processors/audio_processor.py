dca9a5b32b7cbda908145ea0f2d21061e1572431
Tuncay <qtuncay40@gmail.com>
2025-12-09 14:05:58 +0400

    added audio spectrum analyzer to implement processor model

--- processors/audio_processor.py
+++ processors/audio_processor.py
@@ -1,8 +1,10 @@
 import cv2 as cv
 import numpy as np
+from scipy.io import wavfile
+from scipy.fftpack import fft
 
 class AudioProcessor:
-    
+
     def __init__(self, audio_path, sample_rate=44100, buffer_size=1024):
         self.audio_path = audio_path
 
@@ -19,16 +21,24 @@ class AudioProcessor:
             "high" : 1.0
         }
         self.spectral_centroid = 0
-    
+
         self.effect_multipliers = {
             'color_blast': 3.0,
-            'hue_shift': 1.0, 
+            'hue_shift': 1.0,
             'sine_distortion': 1.0
         }
 
     def band_multiplication(self, frame, frequency_band, intensity):
             if frequency_band == "bass":
                 return np.clip(frame * self.frequency_bands["bass"], 0, 255, None)
-        
-                
 
+    def analyze_spectrum(self, frame, audio):
+        sample_rate, audio_data = wavfile.read(audio)
+
+        n = len(audio_data)
+
+        audio_freq = fft(audio_data)
+        audio_freq = audio_freq[0:int(np.ceil((n + 1) / 2.0))]
+
+        magnitude_freq = np.abs(audio_freq)
+        magnitude_freq = magnitude_freq / float(n)
