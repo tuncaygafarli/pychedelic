0af03f2aedc45cc76036120db9a22bd0f97b1be6
Tuncay <68643463+tuncayofficial@users.noreply.github.com>
2025-12-19 12:46:44 +0400

    Update license to GNU General Public License, Version 3

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ Pychedelic - Image and Video Effects Processing Library
 
 __version__ = "1.0.0"
 __author__ = "Tuncay Gafarli"
-__license__ = "MIT"
+__license__ = "GNU GENERAL PUBLIC LICENSE, Version 3"
 
 # Core exports
 from modules.module_manager import ModuleManager
